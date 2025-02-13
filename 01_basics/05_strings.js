a49c7d1b58a49a695c2c692afafde1f2572cbfe3
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-13 10:44:42 +0000

    Numbers and Maths and Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+let name = "Asjad"
+let secondName = " Gull"
+
+
+// console.log(`My First Name is ${name} and my Second Name is ${secondName}`)
+
+const gameName = new String("Asjad")
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+// const myName = "Gull"
+
+// console.log(myName.toUpperCase())
+
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("s"))
+
+console.log(gameName.substring(1,3))
+
+console.log(gameName.slice(-4,2))
+
+let newString = "    Asjad    "
+
+console.log(newString.trim())
+
+const url = "https://asjadgull%20.com"
+
+console.log(url.replace("%20", "-"))
+
+let namee = "Asjad-Gull-Arain"
+
+console.log(namee.split("-"))
