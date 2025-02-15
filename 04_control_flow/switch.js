b2a7b1c8f7125a890e34cce54efac26698ec6da9
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 11:02:39 +0000

    Switch cases in JS

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+month = 3
+
+
+
+switch (month) {
+    case 1:
+        console.log(`JAN`)
+        break;
+     case 2:
+    console.log(`Feb`)
+    break;
+    case 3:
+        console.log(`FEB`)
+        break;
+    default:
+        console.log(`Tell me from the given`)
+        break;
+}
