8df0de0cdf220c4a518a78625b73dcbc8a33539c
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 07:58:04 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,129 @@
+function sayMyName (){
+    console.log("A");
+    console.log("S");
+    console.log("J");
+    console.log("A");
+    console.log("D");
+    
+}
+
+// sayMyName()
+
+
+function addTwoNumbers (number1 , number2){
+// console.log(number1 + number2)
+
+return number1 + number2
+}
+
+// console.log(addTwoNumbers(1 , 2))
+
+
+// function loginUserName (username) {
+//     return `${username} just Logged In `
+// }
+
+
+// console.log(loginUserName("Asjad"))
+
+
+
+// function loginUserName0 (username) {
+//     return `${username} just Logged In `
+      
+// }
+
+// console.log(loginUserName0("Asjad Gull"))
+
+
+
+
+
+// function loginUserName1 (username) {
+//     if(username === undefined){
+//         console.log("Please enter a valid username")
+//         return
+//     }
+//     return `${username} just Logged In `
+      
+// }
+
+// console.log(loginUserName1())
+
+
+
+
+// function loginUserName2(username) {
+//     if(!username){
+//         console.log("Please enter a valid username")
+//         return
+//     }
+//     return `${username} just Logged In `
+      
+// }
+
+
+
+// console.log(loginUserName2())
+
+
+
+
+
+// function loginUserName3(username = "Ash") {
+//     return `${username} just Logged In `
+      
+// }
+
+
+
+// console.log(loginUserName3())
+
+
+
+
+// function loginUserName4(username = "Ash") {
+//     return `${username} just Logged In `
+      
+// }
+
+
+
+// console.log(loginUserName4("Asjad"))
+
+
+
+
+
+
+
+function calculateCartPrice (val1, val2 , ...num1) {
+return num1
+}
+
+// console.log(calculateCartPrice(1213,3424,123,1234,3,1234))
+
+// const user = {
+// name: "Asjad",
+// price: 322
+
+// }
+// function handleObject (anyObject){
+// return `Username is ${anyObject.name} and price is ${anyObject.price}`
+// }
+
+// console.log(handleObject(user))
+
+// console.log(handleObject({
+//     name: "Asjad",
+//     price: 3211
+// }))
+
+
+function returnArray2value (getArray) {
+return `Array's second value is ${getArray[1]}`
+}
+
+// const myArr = [ 111,222,333,444]
+
+console.log(returnArray2value([111,2222,3333,4444]))
