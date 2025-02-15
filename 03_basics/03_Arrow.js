037aba22bc4d594f05ab81706b79b81911d36cd4
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 09:18:43 +0000

    Arrow function in  JS

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,31 @@
+// const user = {
+//     name: "Asjad",
+//     id: 1,
+//     email: "A@gmail.com",
+//     welcomeMessage: function(){
+//         console.log(`${this.name}, welcome to this website`)
+//     }
+// }
+
+// user.welcomeMessage()
+
+// user.name = "Gull"
+// user.welcomeMessage()
+
+
+// const one = () =>{
+//     const fullname = "Asjad Gull"
+//     console.log(fullname)
+// }
+// one()
+
+
+
+// const two = (num1 , num2) =>  num1 + num2
+
+// const two = (num1 , num2) =>  (num1 + num2)
+
+const two = (num1 , num2) =>  ({USERNAME: "aSJAD gULL"})
+
+
+console.log(two(1, 2))
