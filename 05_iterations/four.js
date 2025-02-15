024062c6d9444dff09f1f086d60905155e0a702f
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 13:19:09 +0000

    For in and for of loop in JS

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+// const myObj = {
+//     fb: "Facebook",
+//     sc: "SnapChat",
+//     js: "JavaScript"
+// }
+
+// for (const key  in myObj) {
+//     console.log(myObj[key])
+//     console.log(`${key} and the value is ${myObj[key]}`)
+// }
+
+const myarr = ["a","b " , "c" ]
+
+for (const key in myarr) {
+    console.log(key)
+}
