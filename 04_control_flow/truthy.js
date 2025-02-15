01a75e9d403bc5820939bd51afb68112d8138b6b
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 11:02:50 +0000

    Truthy and falsy in JS

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,67 @@
+// const useremail = "user@email.com"
+
+// if(useremail){
+//     console.log`Have email`
+// }
+// else{
+//     console.log(`dont have any email`)
+// }
+
+//falsy
+
+
+// const user = ''
+// if(user){
+//     console.log`Have email`
+// }
+// else{
+//     console.log(`dont have any email`)
+// }
+
+
+//truthy 
+
+// const user = []
+
+// if(user.length == 0){
+//     console.log(`Its and Empty array `)
+// }else{
+//     console.log(`Not an empty array`)
+// }
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length == 0){
+//     console.log(`Empty Object`)
+// }else{
+//     console.log(`Not an empty Object`)
+// }
+
+
+
+// Nullish operator
+
+
+// let val1;
+// val1 = 5 ?? 10
+
+// console.log(val1)
+
+// let val2;
+// val2 = null ?? 10
+
+// console.log(val2)
+
+
+// let val3;
+// val3 = undefined ?? 10
+
+// console.log(val3)
+
+
+
+//ternary operator
+// condition ? true:false
+
+let iceTea = 100
+iceTea <= 80 ? console.log(`Less than 80`) : console.log(`More than 80`)
