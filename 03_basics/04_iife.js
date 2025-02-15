0d83f74097dc72302e3d1279b39d83e57feceddc
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 09:41:11 +0000

    IIFE in JS

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,18 @@
+// function chai (){
+//     console.log(`DB CONNECTED`)
+// }
+
+// chai()
+
+// (function chai (){
+//     console.log(`DB CONNECTED`)
+// })();
+
+// ( () => {
+//     console.log(`DB CONNECTED TWO`)
+// })()
+
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+})("Asjad")
