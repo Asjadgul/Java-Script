cd726da14a16f4e5c24850d011b8c6fafe3b1d36
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 09:18:22 +0000

    sCOPES IN JS

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,48 @@
+
+
+
+if (true){
+    let a = 1
+    const b = 2
+    var c = 3
+}
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+
+function one(){
+
+    const username = "Asjad"
+    function two(){
+        const website = "youttube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+
+}
+
+one()
+
+
+
+if(true){
+    const username = "Asjad"
+
+    if(username === "Asjad"){
+        const website =" Youtube"
+        console.log( username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+const addtwo = function(num){
+    return num + 1
+}
+console.log(addtwo(5))
