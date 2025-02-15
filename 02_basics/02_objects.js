68fc433cd0eaa46a010d4216a607a780e7656253
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 07:16:45 +0000

    Object destructure

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -36,7 +36,64 @@ const user = {
 
 
 user.greetings = function(){
-    console.log(`Hello Mr, ${this.name}`);
+    // console.log(`Hello Mr, ${this.name}`);
     
 }
-console.log(user.greetings())
+// console.log(user.greetings())
+
+
+const userinfo = {
+    userfullname: {
+        fullname: {
+            firstname: "Asjad",
+            lastname: "Gull"
+        }
+    }
+}
+
+// console.log(userinfo.userfullname.fullname);
+
+const obj1 = {
+    1:"A", 2:"B"
+}
+const obj2 = {
+    3:"A", 4:"B"
+}
+
+// obj3 = Object.assign({}, obj1,obj2)
+
+
+obj3 = {...obj1 , ...obj2}
+// console.log(obj3);
+
+
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 2,
+        email: "b@gmail.com"
+    },
+]
+// console.log(users[0].id)
+
+
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+
+const course = {
+    name: "Java script",
+    price: 999,
+    courseInstructor: "Asjad Gull"
+}
+
+
+const {name : n, price : p, courseInstructor: c} = course
+
+console.log(n , p , c)
