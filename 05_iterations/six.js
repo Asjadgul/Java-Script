a8a6c8c147ea25aa8685112258703d6e164e42fb
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 14:02:13 +0000

    Filter in JS

---
+++ 05_iterations/six.js
@@ -0,0 +1,23 @@
+// const coding = ["js", "swift", "java", "cpp"]
+
+
+// const variable = coding.forEach((item)=> {
+//     console.log(item)
+// })
+
+// console.log(variable)
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(myNums.filter( (items) => items > 5 ))
+// console.log(myNums.filter( (items) => {
+//     return items > 5
+// } ))
+
+
+
+// console.log(myNums.filter( (items) => 
+//     (items > 5) 
+// ))
+
