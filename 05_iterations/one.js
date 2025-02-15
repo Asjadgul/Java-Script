10218579a51b4e9d40c5d85447897bf1cdc359d2
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 11:57:58 +0000

    For loop in JS

---
+++ 05_iterations/one.js
@@ -0,0 +1,43 @@
+// let i = 50
+
+
+// for (let i = 0; i <= 50; i++) {
+//     if(i == 5 ){
+//         console.log(`5 is best`)
+//     }
+//     console.log(i)
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`OUTER LOOP ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`INNER LOOP ${j}`)
+        
+//     }
+// }
+
+
+// const myArr = [1,2,3]
+
+// for (let index = 0; index < myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element)
+// }
+
+
+
+for (let i = 0; i < 10; i++) {
+if(i == 5){
+    break
+}
+    console.log(i);
+    
+}
+for (let i = 0; i < 10; i++) {
+if(i == 5){
+    continue
+}
+    console.log(i);
+    
+}
