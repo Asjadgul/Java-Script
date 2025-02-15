6129881e1a2216c18c1b61ede0206abed46c4841
Asjad Gull <148690534+Asjadgul@users.noreply.github.com>
2025-02-15 11:01:58 +0000

    If else in JS

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+const ifUserLoggedIn = false
+// == , <=, >= , != , < , > , ===
+if(ifUserLoggedIn) {
+    console.log(`Executed`)
+}
+
+
+// const temperature = 50
+// if(temperature> 40){
+//     console.log(`Ok`)
+// }else{
+//     console.log(`You need to move to Germany`)
+// }
+
+// const balance = 1000
+
+// if(balance > 500) console.log(`RICHMAN`);
+// console.log(`V. RichMAN`)
+
+
+// if(balance <500){
+//     console.log(`500`)
+// }
+// else if (balance < 800){
+// console.log(`800`)
+// }
+// else{
+//     console.log(`DAMN RICH  `)
+// }
+
+
+const userLoggedin = true
+
+const debitCard = true
+
+if(userLoggedin && debitCard){
+    console.log(`Can Buy thing now`)
+}
+
+
+const googleloggedIn = true
+const FbLoggedIn = false
+
+if(googleloggedIn || FbLoggedIn){
+    console.log(`You Are Logged In`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
